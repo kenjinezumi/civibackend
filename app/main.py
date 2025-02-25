e0d08ac86f4b05c1d275701440dbdd105628a21f
kenjinezumi <kenjitsuchiya1@gmail.com>
2025-02-25 13:29:05 +0000

    Add api connection

--- app/main.py
+++ app/main.py
@@ -3,9 +3,16 @@ import uvicorn
 from fastapi import FastAPI
 from app.core.config import settings
 from app.routers.form_router import router as form_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.PROJECT_NAME)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # or "http://127.0.0.1:3000"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(form_router)
 
 if __name__ == "__main__":
